1962c0073ec7c7f51db281dfd59f8fd9d2d14708
Magda <magdtrag@gmail.com>
2018-02-05 17:28:34 +0100

    indico.io

---
+++ src/sample/API.java
@@ -0,0 +1,56 @@
+package sample;
+
+import java.util.*;
+import java.net.*;
+
+import io.indico.Indico;
+
+import io.indico.api.utils.IndicoException;
+
+import io.indico.api.results.IndicoResult;
+
+import java.util.Map;
+
+import java.util.ArrayList;
+
+
+public class API {
+
+    ArrayList<Pair<String,Double>> recognise(String imgPath) throws Exception {
+        ArrayList<Pair<String,Double>> output = new ArrayList<Pair<String,Double>>();
+        double max;
+        String maxKey="";
+        String filename;
+        try {
+            Indico indico = new Indico("1558e05956ba46730408f2b2d5b34480");
+            IndicoResult single = indico.imageRecognition.predict(imgPath);
+            Map<String, Double> result = single.getImageRecognition();
+            for (int i = 0; i < 4; i++) {
+                max = Collections.max(result.values());
+                filename = "";
+                for (HashMap.Entry<String, Double> entry : result.entrySet()) {
+                    String key = entry.getKey();
+                    Double value = entry.getValue();
+                    if (max == value) {
+                        maxKey = key;
+                        String [] array = key.split(",",2);
+                        filename = array[0];
+                        Pair<String, Double> pair = new Pair<String, Double>();
+                        pair.first = filename;
+                        pair.second = max;
+                        output.add(pair);
+                    }
+                }
+                result.remove(maxKey);
+            }
+        }catch(IndicoException a){
+            throw new Exception("Invalid API key");
+        }catch(UnknownHostException x){
+            throw new Exception("Internet connection lost");
+        } catch (Exception e){
+            throw e;
+        }
+        return output;
+    }
+}
+
---
+++ src/sample/Controller.java
@@ -0,0 +1,99 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import javafx.scene.control.ListView;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Controller {
+
+    LinkedList<String> helper = new LinkedList<String>();
+    @FXML
+    private ListView listView;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private BarChart<String, Number> chart;
+
+    public void pressButton(ActionEvent event) {
+        listView.getSelectionModel().clearSelection();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Pick a photo");
+        File file = directoryChooser.showDialog(null);
+        if (file != null) {
+            String[] list = null;
+            list = file.list();
+            for (String i : list) {
+                String absolutePath = file.getAbsolutePath() + "\\" + i;
+                if (!(new File(absolutePath)).isDirectory()) {
+                    listView.getItems().add(i);
+                    helper.addLast(absolutePath);
+                }
+            }
+        }
+    }
+
+    public void handleMouseClick(MouseEvent arg) {
+        chart.getData().clear();
+        chart.layout();
+        String absolutePath = "";
+        API recogniser = new API();
+        for (String i : helper) {
+            if (i.contains(listView.getSelectionModel().getSelectedItem().toString())) {
+                absolutePath = i;
+            }
+        }
+        File imgFile = new File(absolutePath);
+        Image img = new Image(imgFile.toURI().toString());
+        imageView.setImage(img);
+        imageView.setFitHeight(200);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
+        imageView.setCache(true);
+
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Probability");
+        chart.setTitle("What is that?");
+        ArrayList<Pair<String, Double>> x1 = new ArrayList<Pair<String, Double>>();
+        XYChart.Series<String, Number> series1 = new XYChart.Series<>();
+        XYChart.Series<String, Number> series2 = new XYChart.Series<>();
+        XYChart.Series<String, Number> series3 = new XYChart.Series<>();
+        XYChart.Series<String, Number> series4 = new XYChart.Series<>();
+        try {
+            x1 = recogniser.recognise(absolutePath);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        series1.getData().add(new XYChart.Data<>(x1.get(0).first, x1.get(0).second));
+        series2.getData().add(new XYChart.Data<>(x1.get(1).first, x1.get(1).second));
+        series3.getData().add(new XYChart.Data<>(x1.get(2).first, x1.get(2).second));
+        series4.getData().add(new XYChart.Data<>(x1.get(3).first, x1.get(3).second));
+        chart.getData().addAll(series1,series2,series3,series4);
+        chart.setAnimated(false);
+
+
+    }
+}
